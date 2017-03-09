38d7b71ba8ed2e67c9204aa0ff5c0b5777db9623
jnraber <jnraber@gmail.com>
2017-03-09 14:53:18 -0500

    Add files via upload

--- VirtualDeobfuscator.py
+++ VirtualDeobfuscator.py
@@ -2,6 +2,17 @@
 # Virtual Deobfuscator (VD)
 # author:  Jason Raber
 # company: HexEffect, LLC www.hexeffect.com  2013
+# Virtual Deobfuscator is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by the Free 
+# Software Foundation, either version 3 of the License, or (at your option) any 
+# later version.
+#
+# Virtual Deobfuscator is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with 
+# Virtual Deobfusctor. If not, see http://www.gnu.org/licenses/.
 # Notes:
 # Make sure to install Python package lxml http://lxml.de/installation.html
 # Python 2.7
